88db1afcfd0876d9d2dbe75ba488bd30107c5b26
Karan Patel <karancpatel5@gmail.com>
2022-04-15 23:49:49 -0400

    Green - Finished MainActivity and now all tests work

--- app/src/main/java/com/example/a4_karan/MainActivity.kt
+++ app/src/main/java/com/example/a4_karan/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.a4_karan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a4_karan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBookNow.setOnClickListener{
+            //check if passport is null or empty and if empty set tvResult to "Sorry, you must provide a passport"
+            if(binding.etPassportNum.text.isNullOrEmpty()){
+                binding.tvResult.text = "Sorry, you must provide a passport"
+            }
+
+            else{
+                binding.tvResult.text = "Flight booked!"
+                binding.tvReservations.text = "1"
+            }
+        }
     }
 }
+
