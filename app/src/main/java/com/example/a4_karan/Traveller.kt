f49e0295ca9c9ca52d2ec5d92d9b0b04dc596b7f
Karan Patel <karancpatel5@gmail.com>
2022-04-15 23:34:25 -0400

    Green - Implemented code in traveller class to make all travellertests to pass

--- app/src/main/java/com/example/a4_karan/Traveller.kt
+++ app/src/main/java/com/example/a4_karan/Traveller.kt
@@ -1,4 +1,22 @@
 package com.example.a4_karan
 
-class Traveller {
+class Traveller (val name: String, val email: String, val bookings: MutableList<Flight>, val passport: String?) {
+
+    fun bookFlight(flight: Flight): Boolean {
+        if (passport != null) {
+            bookings.add(flight)
+            return true
+        }
+        return false
+    }
+
+    val totalCost: Double
+        get() {
+            var total = 0.0
+            for (booking in bookings) {
+                total += booking.cost
+            }
+            return total
+        }
+
 }
