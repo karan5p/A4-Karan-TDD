6e7795cd3eaf8f9f641d6b72ae50761085e74d4f
Karan Patel <karancpatel5@gmail.com>
2022-04-15 23:11:49 -0400

    Green - Added code to Flight class for FlightTest so that all tests pass.

--- app/src/main/java/com/example/a4_karan/Flight.kt
+++ app/src/main/java/com/example/a4_karan/Flight.kt
@@ -1,4 +1,12 @@
 package com.example.a4_karan
 
-class Flight {
+class Flight(  var departingAirport : String? = null, var arrivingAirport : String? = null, var distance : Double? = null, ) {
+    val cost : Double
+        get() {
+            if (distance!! >= 1000) {
+                return 1200 + 0.35 * distance!!
+            } else {
+                return 350 + 0.75 * distance!!
+            }
+        }
 }
